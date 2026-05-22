76d3e47be209d361061c2e499c41a25fdf59fdf4
Andrey Yakushin <a.yakushin@adguard.com>
2026-05-22 12:44:19 +0400

    pimpl for thred dispatcher

--- plugins/vpn_plugin/windows/ui_thread_dispatcher.cpp
+++ plugins/vpn_plugin/windows/ui_thread_dispatcher.cpp
@@ -1,41 +1,61 @@
 #include "ui_thread_dispatcher.h"
 
-UIThreadDispatcher::UIThreadDispatcher() {
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+
+#include <mutex>
+#include <queue>
+
+struct UIThreadDispatcher::Impl {
+  static constexpr UINT WM_DISPATCH = WM_APP + 1;
+  static constexpr const wchar_t kClassName[] = L"VpnPluginDispatcher";
+
+  static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp);
+
+  HWND hwnd = nullptr;
+  std::mutex mutex;
+  std::queue<std::function<void()>> queue;
+};
+
+UIThreadDispatcher::UIThreadDispatcher() : impl_(std::make_unique<Impl>()) {
   WNDCLASSEXW wc = {};
   wc.cbSize = sizeof(wc);
-  wc.lpfnWndProc = WndProc;
-  wc.lpszClassName = kClassName;
+  wc.lpfnWndProc = Impl::WndProc;
+  wc.lpszClassName = Impl::kClassName;
   wc.hInstance = GetModuleHandle(nullptr);
   RegisterClassExW(&wc);
-  hwnd_ = CreateWindowExW(0, kClassName, L"", 0, 0, 0, 0, 0,
+  impl_->hwnd = CreateWindowExW(0, Impl::kClassName, L"", 0, 0, 0, 0, 0,
       HWND_MESSAGE, nullptr, GetModuleHandle(nullptr), nullptr);
-  SetWindowLongPtr(hwnd_, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(this));
+  SetWindowLongPtr(impl_->hwnd, GWLP_USERDATA,
+                   reinterpret_cast<LONG_PTR>(impl_.get()));
 }
 
 UIThreadDispatcher::~UIThreadDispatcher() {
-  if (hwnd_) {
-    DestroyWindow(hwnd_);
+  if (impl_->hwnd) {
+    DestroyWindow(impl_->hwnd);
   }
 }
 
 void UIThreadDispatcher::RunOnUIThread(std::function<void()> task) {
   {
-    std::lock_guard<std::mutex> lock(mutex_);
-    queue_.push(std::move(task));
+    std::lock_guard<std::mutex> lock(impl_->mutex);
+    impl_->queue.push(std::move(task));
   }
-  PostMessage(hwnd_, WM_DISPATCH, 0, 0);
+  PostMessage(impl_->hwnd, Impl::WM_DISPATCH, 0, 0);
 }
 
-LRESULT CALLBACK UIThreadDispatcher::WndProc(HWND hwnd, UINT msg, WPARAM wp,
-                                             LPARAM lp) {
+LRESULT CALLBACK UIThreadDispatcher::Impl::WndProc(HWND hwnd, UINT msg,
+                                                   WPARAM wp, LPARAM lp) {
   if (msg == WM_DISPATCH) {
-    auto* self = reinterpret_cast<UIThreadDispatcher*>(
+    auto* pimpl = reinterpret_cast<Impl*>(
         GetWindowLongPtr(hwnd, GWLP_USERDATA));
-    if (self) {
+    if (pimpl) {
       std::queue<std::function<void()>> tasks;
       {
-        std::lock_guard<std::mutex> lock(self->mutex_);
-        std::swap(tasks, self->queue_);
+        std::lock_guard<std::mutex> lock(pimpl->mutex);
+        std::swap(tasks, pimpl->queue);
       }
       while (!tasks.empty()) {
         tasks.front()();
