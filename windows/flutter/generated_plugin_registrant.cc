5318c6b8ce148d8d3bdc19b32e95bf63246df787
Andrey Yakushin <a.yakushin@adguard.com>
2026-05-29 16:01:15 +0400

    Fix plugin name

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -10,8 +10,8 @@
 #include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
 #include <sqlite3_flutter_libs/sqlite3_flutter_libs_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
-#include <vpn_plugin/vpn_plugin.h>
 #include <window_manager/window_manager_plugin.h>
+#include <vpn_plugin/vpn_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AppLinksPluginCApiRegisterWithRegistrar(
@@ -22,8 +22,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("Sqlite3FlutterLibsPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-  VpnPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("VpnPlugin"));
   WindowManagerPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("WindowManagerPlugin"));
+  VpnPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("VpnPluginCApi"));
 }
