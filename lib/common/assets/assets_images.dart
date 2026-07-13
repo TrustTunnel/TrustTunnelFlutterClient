e617bb535dd75c0380eab56334be6f5388089c80
Stepan Kondrashov <s.kondraschov@adguard.com>
2026-07-13 20:51:20 +0700

    feat (connection log): added images for empty state

--- lib/common/assets/assets_images.dart
+++ lib/common/assets/assets_images.dart
@@ -3,5 +3,9 @@ abstract class AssetImages {
 
   static const about = '${_path}about.png';
 
+  static const connectionLogDesktop = '${_path}connection_log_desktop.png';
+
+  static const connectionLogMobile = '${_path}connection_log_mobile.png';
+
   static const _path = 'assets/images/';
 }
